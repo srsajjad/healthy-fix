708ea123ef4683de8601732e9ac4751fc2a741a1
srsajjad <sajjadsr001@gmail.com>
2018-07-20 18:34:33 +0600

    add init dependencies

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        LOL
+      </div>
+    )
+  }
+}
+
+export default App
